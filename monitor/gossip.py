bdc2587b899736c6f88600ff6d2096f338542a38
Rajeshwari <rajeshwarivk17@gmail.com>
2026-08-19 21:11:25 +0530

    Add node toggle functionality

--- monitor/gossip.py
+++ monitor/gossip.py
@@ -5,9 +5,8 @@ class GossipProtocol:
         while True:
             await asyncio.sleep(5)
             stats = {"cpu": psutil.cpu_percent(), "ram": psutil.virtual_memory().percent, "id": self.node.id}
-            # Broadcast to peers
             # print(f"Gossip: {stats}")
     async def get_best_node(self):
-        # return peer with lowest cpu
+    
         if not self.cluster_info: return None
         return min(self.cluster_info.values(), key=lambda x: x['cpu'])
